d05ecbb7531ddde59c7b943103c21c9a93240a21
elbik <elbikrisafae@gmail.com>
2021-11-17 11:54:10 +0100

    initialcommit

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -7,9 +7,6 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
-
-
-
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,15 +20,12 @@ public class MainActivity extends AppCompatActivity {
         rollButton.setOnClickListener(view -> {
             TextView textView1 =  findViewById(R.id.text1);
             TextView textView2 =  findViewById(R.id.text2);
-            textView1.setText(Integer.toString(getRandomNumber1(1,6)));
-            textView2.setText(Integer.toString(getRandomNumber2(1,6)));
+            textView1.setText(Integer.toString(getRandomNumber(1,6)));
+            textView2.setText(Integer.toString(getRandomNumber(1,6)));
         });
     }
-    public int getRandomNumber1(int min, int max) {
+    public int getRandomNumber(int min, int max) {
         return new Random().nextInt(max-min) + min;
     }
 
-    public int getRandomNumber2(int min,int max){
-        return new Random().nextInt(max-min) + min;
-    }
 }
